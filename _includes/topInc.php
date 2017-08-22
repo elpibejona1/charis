8262d52994919131150271a527fbd310c7cefa44
Jon Guiles <jon.guiles@corporatereport.com>
2017-08-22 12:28:29 -0400

    Josh’s kickbacks, tested links, linked up vimeo videos.

--- _includes/topInc.php
+++ _includes/topInc.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">  
 <title>Charis Alliance</title>
-<link rel="shortcut icon" href="<?php echo $directory ?>_images/favicon.ico?v2" />
+<link rel="shortcut icon" href="<?php echo $directory ?>favicon.ico?v2" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php echo $directory ?>_js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
--- celebration/index.php
+++ celebration/index.php
@@ -5,7 +5,7 @@
 	<? $globalFunction->includeContent(1,'topInc'); ?>
 </head>
 <body class="body--celebration">
-<div class="container">
+<div class="fullContainer fullContainer--noPadding">
     <div class="row">
         <div class="hero">
             <div class="hero__title">
@@ -14,6 +14,8 @@
             </div>
         </div>
     </div>
+</div>
+<div class="container">
     <div class="row row--content">
         <h2 class="heading">Sunday&nbsp;&nbsp;<span>//</span>&nbsp;&nbsp;November 5&nbsp;&nbsp;<span>//</span>&nbsp;&nbsp;20</span>17</h2>
         <p class="copy copy--large">Your church is invited to join with hundreds of others around the world to learn about how God brought Charis into existence and is using this vibrant alliance to bring the hope of Christ to the corners of the&nbsp;globe.</p>
@@ -30,25 +32,29 @@
             <img src="../_images/celebration/resource1.png" alt="">
             <h3>Celebration Guide</h3>
             <p>A guide to Charis Celebration Sunday, along with a recommended schedule</p>
-            <div><a target="_blank" href="">Download</a></div>
+            <!-- <div><a target="_blank" href="">Download</a></div> -->
+            <span>Coming Soon</span>
         </div>
         <div class="resourceThird">
             <img src="../_images/celebration/placeholder.png" alt="">
             <h3>CHARIS CELEBRATION WHITEBOARD VIDEO</h3>
             <p>For churches with video capability</p>
-            <div><a target="_blank" href="">Vimeo</a></div>
+            <!-- <div><a target="_blank" href="">Vimeo</a></div> -->
+            <span>Coming Soon</span>
         </div>
         <div class="resourceThird">
             <img src="../_images/celebration/placeholder.png" alt="">
             <h3>CHARIS CELEBRATION PRINTED BOOKLET</h3>
             <p>For churches without video capability</p>
-            <div><a target="_blank" href="">Download</a></div>
+            <!-- <div><a target="_blank" href="">Download</a></div> -->
+            <span>Coming Soon</span>
         </div>
         <div class="resourceThird">
             <img src="../_images/celebration/placeholder.png" alt="">
             <h3>PRAYER BOOKLET</h3>
             <p>To help you organize your celebration, with 10-minute, 30-minute and one-hour options</p>
-            <div><a target="_blank" href="">Download</a></div>
+            <!-- <div><a target="_blank" href="">Download</a></div> -->
+            <span>Coming Soon</span>
         </div>
         <div class="resourceThird">
             <img src="../_images/celebration/resource5.png" alt="">
@@ -74,8 +80,9 @@
             <img class="wide" src="../_images/celebration/resource8.png" alt="">
             <h3>Special offering giving card</h3>
             <p>For collecting special gifts on Charis Celebration Sunday</p>
-            <div><a target="_blank" href="">With Bleed</a></div>
-            <div><a target="_blank" href="">No Bleed</a></div>
+            <!-- <div><a target="_blank" href="">With Bleed</a></div>
+            <div><a target="_blank" href="">No Bleed</a></div> -->
+            <span>Coming Soon</span>
         </div>
         <div class="resourceThird">
             <img src="../_images/celebration/resource9.png" alt="">
@@ -93,21 +100,22 @@
         <h2 class="heading">Charis Alliance Videos</h2>
         <p class="copy copy--large">To prepare your church for Charis Celebration Sunday, and to create greater awareness about the Charis Alliance, share these videos at your church and in your social networks.</p>
         <ul class="videoList">
-            <li>VIDEO NAME HYPERLINK</li>
-            <li>A short description of the video can go here.</li>
-            <li>VIDEO NAME HYPERLINK</li>
-            <li>A short description of the video can go here.</li>
-            <li>VIDEO NAME HYPERLINK</li>
-            <li>A short description of the video can go here.</li>
-            <li>VIDEO NAME HYPERLINK</li>
-            <li>A short description of the video can go here.</li>
+            <li><a target="_blank" href="https://vimeo.com/229173759">Charis Celebration Sunday 2017 Promo</a></li>
+            <li>Pastors Mike Yoder, Clancy Cruise, and Keith Minier talk about Charis Celebration Sunday and why your church should be involved!</li>
+            <li><a target="_blank" href="https://vimeo.com/153226056">The Charis Alliance</a></li>
+            <li>This video is an introduction and overview of the Charis Alliance, a movement of Grace Brethren Churches around the world. Finalized in early 2016, most footage from this video was shot in the Fall of 2015 at the Charis National Leadership Encounter in Bangkok,&nbsp;Thailand.</li>
+            <li><a target="_blank" href="https://vimeo.com/161481570">The Charis Alliance (short)</a></li>
+            <li>This video is a shortend version of the introduction and overview of the Charis Alliance.</li>
         </ul>
     </div>
     <div class="row">
         <img src="../_images/celebration/watermark.png" alt="" class="watermark">
     </div>
 </div>
-
+<footer class="celebration__footer fullContainer fullContainer--noPadding">
+    <p>Copyright &copy; <?php echo date("Y"); ?> The Charis Alliance. All rights reserved.</p> 
+    <div><a href="mailto: jonguiles@gmail.com">Contact Us</a></div>
+</footer>
 	<? $globalFunction->includeContent(1,'botInc'); ?>
 </body>
 </html>
