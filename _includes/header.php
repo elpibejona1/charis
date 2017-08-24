1db14bba2bc9a834837668157bbda3d5451f1a52
Jon Guiles <jon.guiles@corporatereport.com>
2017-08-24 12:13:30 -0400

    Added news section.

--- _includes/header.php
+++ _includes/header.php
@@ -19,6 +19,7 @@
 						<li><a href="<?php echo $directory ?>english/media/video.php">Video Archive</a></li>
 						<li><a href="<?php echo $directory ?>english/media/photos.php">Photo Archive</a></li>
 					</ul>
+				<li><a href="<?php echo $directory ?>english/news/index.php">News</a></li>
 				<li>
 					<select  onchange="location = this.options[this.selectedIndex].value;">
 						<option selected>English</option>
