6aed8c9095c615afec8580409c1742fa4e0325ab
Jon Guiles <jon.guiles@corporatereport.com>
2017-09-05 15:11:51 -0400

    Formatted Video section.

--- english/media/video.php
+++ english/media/video.php
@@ -12,15 +12,53 @@
 		<div class="row row--archive">
 			<div class="archive__section">
 				<h2>The Charis Alliance</h2>
-				<video src="../../_video/Intro_to_the_Charis_Alliance.mp4" controls></video>
+				<div class="row">
+					<div class="galleryPrev galleryPrev--video">
+						<a href="#">
+							<img src="http://via.placeholder.com/285x160?text=Image+to+come" alt="">
+							<h3>Charis Whiteboard Video</h3>
+							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil sint iste illum voluptatum dolores nam accusantium id voluptatibus quae incidunt?</p>
+						</a>
+					</div>
+					<div class="galleryPrev galleryPrev--video">
+						<a target="_blank" href="https://vimeo.com/153226056">
+							<img src="../../_images/media/videos/video2.jpg" alt="">
+							<h3>The Charis Alliance</h3>
+							<p>This video is an introduction and overview of the Charis Alliance, a movement of Grace Brethren Churches around the world.</p>
+						</a>
+					</div>
+					<div class="galleryPrev galleryPrev--video">
+						<a target="_blank" href="https://vimeo.com/161481570">
+							<img src="../../_images/media/videos/video3.jpg">
+							<h3>The Charis Alliance (Short&nbsp;Version)</h3>
+							<p>This video is a shortend version of the introduction and overview of the Charis Alliance.</p>
+						</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="galleryPrev galleryPrev--video">
+						<a target="_blank" href="">
+							<img src="../../_images/media/videos/video4.jpg" alt="">
+							<h3>Charis Celebration Sunday 2017 Promo</h3>
+							<p>Pastors Mike Yoder, Clancy Cruise, and Keith Minier talk about Charis Celebration Sunday and why your church should be involved!</p>
+						</a>
+					</div>
+					<div class="galleryPrev galleryPrev--video">
+						<a target="_blank" href="">
+							<img src="../../_images/media/videos/video5.jpg" alt="">
+							<h3>Foundations for a Global Charis Identity</h3>
+							<p>Dr. Dave Guiles explains the history and thought process behind the Charis Commitment to a Common Identity</p>
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="row row--archive">
+		<!-- <div class="row row--archive">
 			<div class="archive__section">
 				<h2>2015 CILE</h2>
 				<video src="../../_video/Intro_to_the_Charis_Alliance.mp4" controls></video>
 			</div>
-		</div>
+		</div> -->
 	</div>
 </div>
 	<? $globalFunction->includeContent(2,'footer'); ?>
