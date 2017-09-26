a6fdc92b491072f27e3967dfc6af83491084dc10
Jon Guiles <jon.guiles@corporatereport.com>
2017-09-26 09:47:26 -0400

    Updated English Captions.

--- english/media/photos.php
+++ english/media/photos.php
@@ -15,14 +15,14 @@
 				<div class="row">
 					<div class="galleryPrev">
 						<a class="fancybox" href="#gallery1">
-							<img src="../../_images/media/photos/cile-2015-1/img1.jpg" alt="">
+							<img src="../../_images/media/photos/cile-2015-1/img1.jpg" alt="Delegates and staff arrive in Bangkok for the Charis International Leadership Encounter.">
 							<h3>Saturday, October 31</h3>
 							<p>Delegates arrive in Bangkok</p>
 						</a>
 					</div>
 					<div class="galleryPrev">
 						<a class="fancybox" href="#gallery2">
-							<img src="../../_images/media/photos/cile-2015-2/img1.jpg" alt="">
+							<img src="../../_images/media/photos/cile-2015-2/img1.jpg" alt="The North American contingent gets acquainted with delegates from France.">
 							<h3>Sunday, November 1</h3>
 							<p>Preliminary Activities</p>
 						</a>
@@ -38,21 +38,21 @@
 				<div class="row">
 					<div class="galleryPrev">
 						<a class="fancybox" href="#gallery4">
-							<img src="../../_images/media/photos/cile-2015-4/img1.jpg" alt="">
+							<img src="../../_images/media/photos/cile-2015-4/img1.jpg" alt="Dr. Dave Guiles leads a discussion on the most recent draft of the Commitment to Common Identity.">
 							<h3>Tuesday, November 3</h3>
 							<p>Charis Meetings Day 2</p>
 						</a>
 					</div>
 					<div class="galleryPrev">
 						<a class="fancybox" href="#gallery5">
-							<img src="../../_images/media/photos/cile-2015-5/img1.jpg" alt="">
+							<img src="../../_images/media/photos/cile-2015-5/img1.jpg" alt="Each country is introduced with information about the country and the numbers of the churches in each area. Gifts are also presented from the country.">
 							<h3>Wednesday, November 4</h3>
 							<p>Charis Meetings Day 3</p>
 						</a>
 					</div>
 					<div class="galleryPrev">
 						<a class="fancybox" href="#gallery6">
-							<img src="../../_images/media/photos/cile-2015-6/img1.jpg" alt="The conference climbed aboard a bus to tour sites in Bangkok.">
+							<img src="../../_images/media/photos/cile-2015-6/img1.jpg" alt="The conference delegates climb aboard a bus to tour sites in Bangkok.">
 							<h3>Thursday, November 5</h3>
 							<p>Bangkok Tour</p>
 						</a>
@@ -83,16 +83,16 @@
 	data-cycle-caption-template="{{alt}}">
 		<div class="slideControls slideControls--left" id=prev1><i class="fa fa-chevron-left"></i></div>
     	<div class="slideControls slideControls--right" id=next1><i class="fa fa-chevron-right"></i></div>
-	    <img src="../../_images/media/photos/cile-2015-1/img1.jpg" alt="Delegates and staff are arriving in Bangkok this weekend for the Charis International Leadership Encounter.">
-	    <img src="../../_images/media/photos/cile-2015-1/img2.jpg" alt="Delegates and staff greeted each other in the lobby of the hotel.">
-	    <img src="../../_images/media/photos/cile-2015-1/img3.jpg" alt="A group set out Saturday night for dinner with Reiner Ehmann from Germany leading the way.">
-	    <img src="../../_images/media/photos/cile-2015-1/img4.jpg" alt="This group tried Thai food at the local mall.">
+	    <img src="../../_images/media/photos/cile-2015-1/img1.jpg" alt="Delegates and staff arrive in Bangkok for the Charis International Leadership Encounter.">
+	    <img src="../../_images/media/photos/cile-2015-1/img2.jpg" alt="Delegates and staff greet each other in the lobby of the hotel.">
+	    <img src="../../_images/media/photos/cile-2015-1/img3.jpg" alt="A group sets out Saturday night for dinner with Rainer Ehmann from Germany leading the way.">
+	    <img src="../../_images/media/photos/cile-2015-1/img4.jpg" alt="This group tries Thai food at the local mall.">
 	    <img src="../../_images/media/photos/cile-2015-1/img5.jpg" alt="Part of the group that tried Thai food at the local mall.">
 	    <img src="../../_images/media/photos/cile-2015-1/img6.jpg" alt="Part of the group that tried Thai food at the local mall.">
 	    <img src="../../_images/media/photos/cile-2015-1/img7.jpg" alt="Part of the group that tried Thai food at the local mall.">
 	    <img src="../../_images/media/photos/cile-2015-1/img8.jpg" alt="Leaving the mall.">
 	    <img src="../../_images/media/photos/cile-2015-1/img9.jpg" alt="A view from the hotel about dusk.">
-	    <img src="../../_images/media/photos/cile-2015-1/img10.jpg" alt="A Saturday night view from the hotel where the CILE meetings are being held.">
+	    <img src="../../_images/media/photos/cile-2015-1/img10.jpg" alt="A Saturday night view from the hotel where the CILE meetings are held.">
 	</div>
 	<!-- empty element for caption -->
 	<div id="alt-caption1" class="slideshow__caption"></div>
@@ -111,16 +111,16 @@
     	<div class="slideControls slideControls--right" id=next2><i class="fa fa-chevron-right"></i></div>
 	   		<img src="../../_images/media/photos/cile-2015-2/img1.jpg" alt="The North American contingent gets acquainted with delegates from France.">
 		    <img src="../../_images/media/photos/cile-2015-2/img2.jpg" alt="Breakfast discussions on Sunday.">
-		    <img src="../../_images/media/photos/cile-2015-2/img3.jpg" alt="Several delegates gathered to discuss a video that will be produced to be shown at national conference in 2016.">
-		    <img src="../../_images/media/photos/cile-2015-2/img4.jpg" alt="Several delegates gathered to discuss a video that will be produced to be shown at national conference in 2016.">
+		    <img src="../../_images/media/photos/cile-2015-2/img3.jpg" alt="Several delegates gather to discuss a video that will be produced to be shown at the American national conference in 2016.">
+		    <img src="../../_images/media/photos/cile-2015-2/img4.jpg" alt="Several delegates gather to discuss a video that will be produced to be shown at the American national conference in 2016.">
 		    <img src="../../_images/media/photos/cile-2015-2/img5.jpg" alt="Delegates get acquainted before dinner.">
 		    <img src="../../_images/media/photos/cile-2015-2/img6.jpg" alt="Delegates get acquainted before dinner.">
 		    <img src="../../_images/media/photos/cile-2015-2/img7.jpg" alt="Delegates get acquainted before dinner.">
 		    <img src="../../_images/media/photos/cile-2015-2/img8.jpg" alt="Delegates get acquainted before dinner.">
-		    <img src="../../_images/media/photos/cile-2015-2/img9.jpg" alt="There were several options for dinner on Sunday night: Thai, American, Japanese, Mexican, and Chinese. Delegates and staff gathered in the lobby then went to the restaurant of their choice.">
+		    <img src="../../_images/media/photos/cile-2015-2/img9.jpg" alt="There are several options for dinner on Sunday night: Thai, American, Japanese, Mexican, and Chinese. Delegates and staff gather in the lobby then go to the restaurant of their choice.">
 		    <img src="../../_images/media/photos/cile-2015-2/img10.jpg" alt="Wayne Hannah and Malcom Stephens try a Hot Pot Japanese meal.">
-		    <img src="../../_images/media/photos/cile-2015-2/img11.jpg" alt="The Charis meetings are being held in the Vertical Suites Hotel in Bangkok.">
-		    <img src="../../_images/media/photos/cile-2015-2/img12.jpg" alt="Mboi Andre and Dave Guiles.">
+		    <img src="../../_images/media/photos/cile-2015-2/img11.jpg" alt="The Charis meetings are held in the Vertical Suites Hotel in Bangkok.">
+		    <img src="../../_images/media/photos/cile-2015-2/img12.jpg" alt="Dr. Mboi Andre (Central African Republic) and Dr. Dave Guiles (Encompass World Partners).">
 	</div>
 	<!-- empty element for caption -->
 	<div id="alt-caption2" class="slideshow__caption"></div>
@@ -137,18 +137,18 @@
 	data-cycle-caption-template="{{alt}}">
 		<div class="slideControls slideControls--left" id=prev3><i class="fa fa-chevron-left"></i></div>
     	<div class="slideControls slideControls--right" id=next3><i class="fa fa-chevron-right"></i></div>
-		    <img src="../../_images/media/photos/cile-2015-3/img1.jpg" alt="Each country was introduced with information about the country and the numbers of the churches in each area. Gifts were also presented from the country.">
-		    <img src="../../_images/media/photos/cile-2015-3/img2.jpg" alt="Each country was introduced with information about the country and the numbers of the churches in each area. Gifts were also presented from the country.">
-		    <img src="../../_images/media/photos/cile-2015-3/img3.jpg" alt="Each country was introduced with information about the country and the numbers of the churches in each area. Gifts were also presented from the country.">
+		    <img src="../../_images/media/photos/cile-2015-3/img1.jpg" alt="Each country is introduced with information about the country and the numbers of the churches in each area. Gifts are also presented from the country.">
+		    <img src="../../_images/media/photos/cile-2015-3/img2.jpg" alt="Each country is introduced with information about the country and the numbers of the churches in each area. Gifts are also presented from the country.">
+		    <img src="../../_images/media/photos/cile-2015-3/img3.jpg" alt="Each country is introduced with information about the country and the numbers of the churches in each area. Gifts are also presented from the country.">
 		    <img src="../../_images/media/photos/cile-2015-3/img4.jpg" alt="Bartley Sawatsky shares about the Grace Brethren movement in Canada.">
-		    <img src="../../_images/media/photos/cile-2015-3/img5.jpg" alt="Keith Minier reports on chruches in the United States.">
+		    <img src="../../_images/media/photos/cile-2015-3/img5.jpg" alt="Keith Minier reports on churches in the United States.">
 		    <img src="../../_images/media/photos/cile-2015-3/img6.jpg" alt="Romtomne Louis shares about the African countries.">
-		    <img src="../../_images/media/photos/cile-2015-3/img7.jpg" alt="Representatives from the Central African Republic share about chruches in their country.">
+		    <img src="../../_images/media/photos/cile-2015-3/img7.jpg" alt="Representatives from the Central African Republic share about churches in their country.">
 		    <img src="../../_images/media/photos/cile-2015-3/img8.jpg" alt="Augustin Hibaile tells about churches in the Central African Republic.">
-		    <img src="../../_images/media/photos/cile-2015-3/img9.jpg" alt="The African contingent shared a skit about ministries in their countries.">
-		    <img src="../../_images/media/photos/cile-2015-3/img10.jpg" alt="The African contingent shared a skit about ministries in their countries.">
-		    <img src="../../_images/media/photos/cile-2015-3/img11.jpg" alt="The African contingent shared a skit about ministries in their countries.">
-		    <img src="../../_images/media/photos/cile-2015-3/img12.jpg" alt="Worship was led by Kevin Stauffer, Paul Klawitter, and Florent Varak.">
+		    <img src="../../_images/media/photos/cile-2015-3/img9.jpg" alt="The African contingent shares a skit about ministries in their countries.">
+		    <img src="../../_images/media/photos/cile-2015-3/img10.jpg" alt="The African contingent shares a skit about ministries in their countries.">
+		    <img src="../../_images/media/photos/cile-2015-3/img11.jpg" alt="The African contingent shares a skit about ministries in their countries.">
+		    <img src="../../_images/media/photos/cile-2015-3/img12.jpg" alt="Worship is led by Kevin Stauffer, Paul Klawitter, and Florent Varak (Encompass World Partners).">
 		    <img src="../../_images/media/photos/cile-2015-3/img13.jpg" alt="The 2015 CILE Delegates">
 	</div>
 	<!-- empty element for caption -->
@@ -166,15 +166,15 @@
 	data-cycle-caption-template="{{alt}}">
 		<div class="slideControls slideControls--left" id=prev4><i class="fa fa-chevron-left"></i></div>
     	<div class="slideControls slideControls--right" id=next4><i class="fa fa-chevron-right"></i></div>
-	   		<img src="../../_images/media/photos/cile-2015-4/img1.jpg" alt="Dave Guiles leads discussion on the most recent draft of the Commitment to Common Identity.">
-		    <img src="../../_images/media/photos/cile-2015-4/img2.jpg" alt="Members of the steering committee fielded questions about the Commitment to Common Identity.">
-		    <img src="../../_images/media/photos/cile-2015-4/img3.jpg" alt="Members of the steering committee fielded questions about the Commitment to to Common Identity.">
-		    <img src="../../_images/media/photos/cile-2015-4/img4.jpg" alt="In the afternoon, the delegates broke into language groups to discuss the Commitment to Common Identity. This is the Spanish language group.">
-		    <img src="../../_images/media/photos/cile-2015-4/img5.jpg" alt="In the afternoon, the delegates broke into language groups to discuss the Commitment to Common Identity. This is the Spanish language group.">
-		    <img src="../../_images/media/photos/cile-2015-4/img6.jpg" alt="In the afternoon, the delegates broke into language groups to discuss the Commitment to Common Identity. This is the French language group.">
-		    <img src="../../_images/media/photos/cile-2015-4/img7.jpg" alt="In the afternoon, the delegates broke into language groups to discuss the Commitment to Common Identity. This is the French language group.">
-		    <img src="../../_images/media/photos/cile-2015-4/img8.jpg" alt="In the afternoon, the delegates broke into language groups to discuss the Commitment to Common Identity. This is the English language group.">
-		    <img src="../../_images/media/photos/cile-2015-4/img9.jpg" alt="In the afternoon, the delegates broke into language groups to discuss the Commitment to Common Identity. This is the English language group.">
+	   		<img src="../../_images/media/photos/cile-2015-4/img1.jpg" alt="Dr. Dave Guiles leads a discussion on the most recent draft of the Commitment to Common Identity.">
+		    <img src="../../_images/media/photos/cile-2015-4/img2.jpg" alt="Members of the steering committee field questions about the Commitment to Common Identity.">
+		    <img src="../../_images/media/photos/cile-2015-4/img3.jpg" alt="Members of the steering committee field questions about the Commitment to Common Identity.">
+		    <img src="../../_images/media/photos/cile-2015-4/img4.jpg" alt="In the afternoon, the delegates break into language groups to discuss the Commitment to Common Identity. This is the Spanish language group.">
+		    <img src="../../_images/media/photos/cile-2015-4/img5.jpg" alt="In the afternoon, the delegates break into language groups to discuss the Commitment to Common Identity. This is the Spanish language group.">
+		    <img src="../../_images/media/photos/cile-2015-4/img6.jpg" alt="In the afternoon, the delegates break into language groups to discuss the Commitment to Common Identity. This is the French language group.">
+		    <img src="../../_images/media/photos/cile-2015-4/img7.jpg" alt="In the afternoon, the delegates break into language groups to discuss the Commitment to Common Identity. This is the French language group.">
+		    <img src="../../_images/media/photos/cile-2015-4/img8.jpg" alt="In the afternoon, the delegates break into language groups to discuss the Commitment to Common Identity. This is the French language group.">
+		    <img src="../../_images/media/photos/cile-2015-4/img9.jpg" alt="In the afternoon, the delegates break into language groups to discuss the Commitment to Common Identity. This is the French language group.">
 	</div>
 	<!-- empty element for caption -->
 	<div id="alt-caption4" class="slideshow__caption"></div>
@@ -216,14 +216,14 @@
 	data-cycle-caption-template="{{alt}}">
 		<div class="slideControls slideControls--left" id=prev6><i class="fa fa-chevron-left"></i></div>
     	<div class="slideControls slideControls--right" id=next6><i class="fa fa-chevron-right"></i></div>
-	   		<img src="../../_images/media/photos/cile-2015-6/img1.jpg" alt="The conference climbed aboard a bus to tour sites in Bangkok.">
-		    <img src="../../_images/media/photos/cile-2015-6/img2.jpg" alt="The delegates from the Fellowship of Grace Brethren Churches: Mike Yoder, Clancy Cruise, Keith Minier, and Bartley Sawatsky.">
+	   		<img src="../../_images/media/photos/cile-2015-6/img1.jpg" alt="The conference delegates climb aboard a bus to tour sites in Bangkok.">
+		    <img src="../../_images/media/photos/cile-2015-6/img2.jpg" alt="The delegates from the American and Canadian churches: Mike Yoder, Clancy Cruise, Keith Minier, and Bartley Sawatsky.">
 		    <img src="../../_images/media/photos/cile-2015-6/img3.jpg" alt="The group heads into one of the sites they visited.">
-		    <img src="../../_images/media/photos/cile-2015-6/img4.jpg" alt="After lunch along the river, some members of the group showed off their newly-purchased Thailand hats.">
+		    <img src="../../_images/media/photos/cile-2015-6/img4.jpg" alt="After lunch along the river, some members of the group show off their newly-purchased Thai hats.">
 		    <img src="../../_images/media/photos/cile-2015-6/img5.jpg" alt="Delegates pose with one of their tour guides on Thursday afternoon.">
-		    <img src="../../_images/media/photos/cile-2015-6/img6.jpg" alt="Taking a break from the tour are Florant Varak, a member of the steering committee from France, and Shu Aoki, a delegate and steering committee member from Japan.">
+		    <img src="../../_images/media/photos/cile-2015-6/img6.jpg" alt="Taking a break from the tour are Florent Varak, a member of the steering committee from France, and Shu Aoki, a delegate and steering committee member from Japan.">
 		    <img src="../../_images/media/photos/cile-2015-6/img7.jpg" alt="Arnulfo Vieyra, Mexico; Kevin Stauffer, France; Franck Segonne, France; Dave Guiles, Encompass World Partners; Paris Pena, Guatemala; and Juan Saez, Chile.">
-		    <img src="../../_images/media/photos/cile-2015-6/img8.jpg" alt="Dr. Augustin Bible, Central African Republic, and Shu Aoki, Japan, listen carefully to the tour guide on Thursday.">
+		    <img src="../../_images/media/photos/cile-2015-6/img8.jpg" alt="Dr. Augustin Hibaile (Central African Republic) and Shu Aoki (Japan) listen carefully to the tour guide on Thursday.">
 	</div>
 	<!-- empty element for caption -->
 	<div id="alt-caption6" class="slideshow__caption"></div>
