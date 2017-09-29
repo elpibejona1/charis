ec209a1615e6db7531f4f6e09f2646d1b1949f52
Jon Guiles <jon.guiles@corporatereport.com>
2017-09-29 16:32:54 -0400

    Added missing French Video.

--- fr/media/video.php
+++ fr/media/video.php
@@ -27,6 +27,13 @@
 							<p>Cette vidéo est une introduction à l'Alliance Charis, un mouvement des Églises des Frères à travers le monde.</p>
 						</a>
 					</div>
+					<div class="galleryPrev galleryPrev--video">
+						<a target="_blank" href="https://vimeo.com/148661909">
+							<img src="../../_images/media/videos/video6.jpg" alt="">
+							<h3>Fondements pour une identité Charis globale</h3>
+							<p>Le Dr. Dave Guiles explique l'histoire et le processus de réflexion derrière l'engagement de Charis à une identité commune.</p>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
