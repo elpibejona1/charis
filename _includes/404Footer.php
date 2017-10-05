be6bb194001f93bd240c51d7de6b8a776ec36faa
Jon Guiles <jon.guiles@corporatereport.com>
2017-10-05 11:28:34 -0400

    File Cleanup.

--- _includes/404Footer.php
+++
@@ -1,9 +0,0 @@
-<div class="footer__image fullContainer">
-</div>
-<footer class="fullContainer footer--404">
-	<div class="container">
-		<div class="row">
-			<div class="siteMap__img siteMap__img--404"></div>
-		</div>
-	</div>
-</footer>
--- _includes/404Header.php
+++
@@ -1,5 +0,0 @@
-<header>
-	<div class="container">
-		<a href="<?php echo $directory ?>index.php"><img src="<?php echo $directory ?>_images/global/logo.png"/ class="header__logo"></a>
-	</div>
-</header>
