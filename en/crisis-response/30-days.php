6fca4b3c31df358b760649b21ea32c974c5fdabd
Jon Guiles <>
2020-03-26 16:13:10 -0400

    30 Days Page.

---
+++ en/crisis-response/30-days.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<?php require("../../_includes/directory.php"); ?>
+<html>
+<head>
+	<?php $globalFunction->includeContent(2,'englishTopInc'); ?>
+</head>
+<body>
+	<?php $globalFunction->includeContent(2,'englishHeader'); ?>
+<div class="documents__background fullContainer">
+	<div class="container">
+		<div class="row">
+			<div class="thirtyDaysContent">
+				<img src="../../_images/crisis-response/30-days/image1.jpg" alt="" class="coverImg">
+				<h1 class="thirtyDays__heading">30 Days to a More Resilient Faith: <br>Embracing the God of the Storm</h1>
+				<p class="thirtyDays__text">The oldest book of the Bible, Job, deals with the oldest question of man: why do seemingly bad things happen to good people? Or put another way, if God is good, why is there suffering?</p>
+				<p class="thirtyDays__text"><strong>Current Chapter -</strong> <a target="_blank" href="../../documents/crisis-response/30-days/english/Day1.pdf"><strong>Day 1:</strong> The Crucible of&nbsp;Crisis</a></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="archive__section">
+				<h2>Download Chapters:</h2>
+			</div>
+			<div class="archive">
+				<div class="archive__files">
+					<ul>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day1.pdf"><strong>Day 1 -</strong>  The Crucible of Crisis</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day2.pdf"><strong>Day 2 -</strong> God Uses Crisis to Move People – Geographically</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day3.pdf"><strong>Day 3 -</strong> God Uses Crisis to Move People – Changing Hearts</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day4.pdf"><strong>Day 4 -</strong> God Uses Crisis to Move People – Changing Minds</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day5.pdf"><strong>Day 5 -</strong> The WHYs and WHATs Behind Suffering</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day6.pdf"><strong>Day 6 -</strong> All That We Don’t know About Trouble</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day7.pdf"><strong>Day 7 -</strong> The Uncomfortable Truth about Trouble: God Could Stop It!</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day8.pdf"><strong>Day 8 -</strong> The Uncomfortable Truth about Trouble: God is Complicit!</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day9.pdf"><strong>Day 9 -</strong> The Strategic Place of Prayer in Crisis</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day10.pdf"><strong>Day 10 -</strong> A Crisis Prayer God Didn’t Forget</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day11.pdf"><strong>Day 11 -</strong> The Blessing of ISIS</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day12.pdf"><strong>Day 12 -</strong> Groaning Spoken Here</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day13.pdf"><strong>Day 13 -</strong> (Groaning): An Aria Breaks Forth</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day14.pdf"><strong>Day 14 -</strong> Cleaning House – Chaff, Dross, and Dust</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day15.pdf"><strong>Day 15 -</strong> An Argument from Silence: The <br>Un-Prayer</a></li>
+					</ul>
+				</div>
+				<div class="archive__files">
+					<ul>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day16.pdf"><strong>Day 16 -</strong> No Graven Image</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day17.pdf"><strong>Day 17 -</strong> No Graven Image – Crises of Faith</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day18.pdf"><strong>Day 18 -</strong> No Graven Image – Obedience is The Highest Service</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day19.pdf"><strong>Day 19 -</strong> No Graven Image – Which Image Will We Embrace?</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day20.pdf"><strong>Day 20 -</strong> Sometimes Knowing What the Reason <em>Is Not</em> Is Enough</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day21.pdf"><strong>Day 21 -</strong> Survivor’s Guide to Suffering – Wonder</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day22.pdf"><strong>Day 22 -</strong> Survivor’s Guide to Suffering – Worship</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day23.pdf"><strong>Day 23 -</strong> Survivor’s Guide to Suffering – Staying Under His Wings</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day24.pdf"><strong>Day 24 -</strong> Splash Overs of Hell</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day25.pdf"><strong>Day 25 -</strong> Prayer – When the Heavens are Like Brass</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day26.pdf"><strong>Day 26 -</strong> Prayer – Worship in Reverent Submission</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day27.pdf"><strong>Day 27 -</strong> Watch Your Mouth! – Job</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day28.pdf"><strong>Day 28 -</strong> Watch Your Mouth! – Jeremiah & Jonah</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day29.pdf"><strong>Day 29 -</strong> Our Happy God</a></li>
+						<li><a target="_blank" href="../../documents/crisis-response/30-days/english/Day30.pdf"><strong>Day 30 -</strong> The Parable of the Little Kite</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+	<?php $globalFunction->includeContent(2,'englishFooter'); ?>
+	<?php $globalFunction->includeContent(2,'botInc'); ?>
+</body>
+</html>
