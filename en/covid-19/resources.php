addcd534c258dce79cc0c653300d7b0c6defd406
Jon Guiles <>
2020-04-08 09:16:48 -0400

    New Resources for english page.

--- en/covid-19/resources.php
+++ en/covid-19/resources.php
@@ -34,6 +34,86 @@
 				</div>
 			</div>
 		</div>
+		<!-- <div class="row row--documents">
+			<div class="interior__photo">
+				<img src="../../_images/crisis-response/resources/thumbnail2.jpg" alt="">
+			</div>
+			<div class="interior__description">
+				<p><span>Covid-19 Hotline:</span> <br>access to professional health care workers in Charis Alliance’s global network.</p>
+				<div class="primary__downloadContainer">
+					<ul>
+						<li class="primary__download__button"><a target="_blank" href="../../documents/crisis-response/resources/Ten_Perspectives_on_Covid19.pdf">Download</a></li>
+					</ul>
+				</div>
+			</div>
+		</div> -->
+		<div class="row row--documents">
+			<div class="interior__photo">
+				<img src="../../_images/crisis-response/resources/thumbnail3.jpg" alt="">
+			</div>
+			<div class="interior__description">
+				<p><span>Preparing Your Church for Covid-19 (a Manual):</span> <br>A Step-by-Step, Research-Informed and Faith-Based Planning Manual by Wheaton College's Humanitarian Disaster Institute.</p>
+				<div class="primary__downloadContainer">
+					<ul>
+						<li class="primary__download__button"><a target="_blank" href="../../documents/crisis-response/resources/Preparing-Your-Church-for-Coronavirus.pdf">Download</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row row--documents">
+			<div class="interior__photo">
+				<img src="../../_images/crisis-response/resources/thumbnail4.jpg" alt="">
+			</div>
+			<div class="interior__description">
+				<p><span>Coronavirusandthechurch.com</span> <br>Resources to assist churches and ministry leaders as they prepare for and respond to the effects of the Coronavirus in their congregations and communities.</p>
+				<div class="primary__downloadContainer">
+					<ul>
+						<li class="primary__download__button"><a target="_blank" href="https://coronavirusandthechurch.com/">View Resource</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="row row--documents">
+			<div class="interior__photo">
+				<img src="../../_images/crisis-response/resources/thumbnail5.jpg" alt="">
+			</div>
+			<div class="interior__description">
+				<p><span>Covid-19 Webinars:</span> <br>Video webinars from Wheaton College's Humanitarian Disaster Institute including "Preparing Your Church for the Coronavirus" and "Coronavirus and Self Care."</p>
+				<div class="primary__downloadContainer">
+					<ul>
+						<li class="primary__download__button"><a target="_blank" href="https://www.wheaton.edu/academics/academic-centers/humanitarian-disaster-institute/covid-19/hdi-covid-19-webinar-archive/">View Resource</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row row--documents">
+			<div class="interior__photo">
+				<img src="../../_images/crisis-response/resources/thumbnail6.jpg" alt="">
+			</div>
+			<div class="interior__description">
+				<p><span>Covid-19 - Fact vs Fiction</span>: <br>Mythbusters and advice for the general public from the World Health Organization.</p>
+				<div class="primary__downloadContainer">
+					<ul>
+						<li class="primary__download__button"><a target="_blank" href="https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public/myth-busters">View Resource</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+		<div class="row row--documents">
+			<div class="interior__photo">
+				<img src="../../_images/crisis-response/resources/thumbnail6.jpg" alt="">
+			</div>
+			<div class="interior__description">
+				<p><span>WHO Coronavirus Information:</span>: <br>Facts on the coronavirus from the World Health Organization. Includes a general overview, common symptoms, and recommendations for prevention.</p>
+				<div class="primary__downloadContainer">
+					<ul>
+						<li class="primary__download__button"><a target="_blank" href="https://www.who.int/health-topics/coronavirus">View Resource</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 
